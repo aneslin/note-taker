9812702a6784e41c04019e9188d95ebebf038017
Aaron Neslin <aneslin125@gmail.com>
2022-02-11 16:40:02 -0500

    added delete route and uniqid

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const { notes } = require('./db/db.json')
 
 const fs = require('fs')
 
+const uniqid = require('uniqid')
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static('public'))
@@ -24,23 +26,38 @@ function newNote (note, noteArray) {
   return note
 }
 
+function deleteNote( noteId, noteArray) {
+  noteArray.pop(noteId)
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify({ notes: noteArray }, null, 2)
+  )
+}
+
 app.get('/api/notes', (req, res) => {
   const results = notes
   res.json(results)
 })
 
 app.post('/api/notes', (req, res) => {
-  req.body.id = notes.length.toString()
+  req.body.id = uniqid()
   newNote(req.body, notes)
   res.json(req.body)
 })
 
+app.delete('/api/notes/:id', (req, res) => {
+  const result = deleteNote(req.params.id, notes);
+  if (result) {
+    res.json(result + ' Deleted')
+  } else {
+    res.sendStatus(404)
+  };
+})
 
 // html route
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'))
 })
-
 
 app.listen(PORT, () => {
   console.log(`Server is now on port ${PORT}`)
