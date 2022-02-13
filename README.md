1300d4c75c0c7285538b75b4aae2143541c6f813
Aaron Neslin <aneslin125@gmail.com>
2022-02-13 14:01:24 -0500

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# note-taker
+# Note Taker
+
+## Description
+
+Create reminders for yourself.  Enter note title and note text, and click the save button. To delete a note, click the trash can.
+
+## Tools
+
+* Nodejs
+* Express
+*  uniqid 
+* NPM
+
+
+## Steps
+1. Click on the + to create a new note
+1. Enter note title
+1. Enter note text
+1. Save note
+
+To delete, select the note and click the trash icon
+
+
+Deployed URL:
+
--- server.js
+++ server.js
@@ -1,64 +1,67 @@
+const express = require("express");
 
-const express = require('express')
+const PORT = process.envPORT || 3001;
 
-const PORT = process.envPORT || 3001
+const path = require("path");
 
-const path = require('path')
+const app = express();
 
-const app = express()
+const { notes } = require("./db/db.json");
 
-const { notes } = require('./db/db.json')
+const fs = require("fs");
 
-const fs = require('fs')
+const uniqid = require("uniqid");
 
-const uniqid = require('uniqid')
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(express.static('public'))
-
-function newNote (note, noteArray) {
-  noteArray.push(note)
+function newNote(note, noteArray) {
+  noteArray.push(note);
   fs.writeFileSync(
-    path.join(__dirname, './db/db.json'),
+    path.join(__dirname, "./db/db.json"),
     JSON.stringify({ notes: noteArray }, null, 2)
-  )
-  return note
+  );
+  return note;
 }
 
-function deleteNote( noteId, noteArray) {
-  noteArray.pop(noteId)
+function deleteNote(noteId, noteArray) {
+  noteArray.pop(noteId);
   fs.writeFileSync(
-    path.join(__dirname, './db/db.json'),
+    path.join(__dirname, "./db/db.json"),
     JSON.stringify({ notes: noteArray }, null, 2)
-  )
+  );
 }
 
-app.get('/api/notes', (req, res) => {
-  const results = notes
-  res.json(results)
-})
+app.get("/api/notes", (req, res) => {
+  const results = notes;
+  res.json(results);
+});
 
-app.post('/api/notes', (req, res) => {
-  req.body.id = uniqid()
-  newNote(req.body, notes)
-  res.json(req.body)
-})
+app.post("/api/notes", (req, res) => {
+  req.body.id = uniqid();
+  newNote(req.body, notes);
+  res.json(req.body);
+});
 
-app.delete('/api/notes/:id', (req, res) => {
+app.delete("/api/notes/:id", (req, res) => {
   const result = deleteNote(req.params.id, notes);
   if (result) {
-    res.json(result + ' Deleted')
+    res.json(result + " Deleted");
   } else {
-    res.sendStatus(404)
-  };
-})
+    res.sendStatus(404);
+  }
+});
 
 // html route
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'))
-})
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
 
 app.listen(PORT, () => {
-  console.log(`Server is now on port ${PORT}`)
-})
+  console.log(`Server is now on port ${PORT}`);
+});
